6d787c6cac603beaf2e2ca565b3178e75d51b5c5
Angelica <maria.alfaro@mail.escuelaing.edu.co>
2020-08-29 08:37:52 -0500

    especificación métodos

---
+++ src/main/java/hangman/model/PowerBonusScore.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+public class PowerBonusScore {
+	
+	/**
+	 * @pre calcular el puntaje a partir del número de letras correctas (+10) e incorrectas (-5)
+	 * @param correctCount, número de letras correctas
+	 * @param incorrectCount, número de letras incorrectas
+	 * @return puntaje calculado 
+	 */
+	public int calculateScore(int correctCount, int incorrectCount) {
+		
+		return 3;
+	}
+
+}
